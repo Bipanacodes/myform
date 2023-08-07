05b7b191f42ac0ccbadaad09121602381c5c6983
Bipana2055 <thapabipana828@gmail.com>
2023-08-07 20:27:38 +0545

    simple log in form

---
+++ script.js
@@ -0,0 +1,11 @@
+const signInBtn = document.querySelector('signInBtn-link');
+const signUpBtn = document.querySelector('signupBtn-link');
+const wrapper = document.querySelector('.wrapper');
+
+signUpBtnlink.addEventListener('click',() => {
+    wrapper.classList.toggle('active');
+});
+signUpBtnlink.addEventListener('click',() => {
+    wrapper.classList.toggle('active');
+    
+});
